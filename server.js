32ea711f141c4d3b7605ba14dfe5e0fb60d17caa
Yuvraj Singh <yuvraj0157@gmail.com>
2024-05-25 18:21:51 +0530

    Initial project setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+    }
+);
